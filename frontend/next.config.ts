8890d65e5342275d1fa580741b0f8aa8a38fc527
Kariwo Ibanoshi <kariwoibanoshi@gmail.com>
2026-07-20 01:59:08 +0530

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,14 +1,13 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  output: "standalone",
+const nextConfig = {
   reactStrictMode: false,
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
-  }
+  },
 };
 
 export default nextConfig;
